bfaa63c264b46267bcc21e42625dc878f574f535
Simon Weller <s.meierbeck@gmail.com>
2022-05-02 11:03:32 +0200

    Add basic styling for question list

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import useQuestions from './App.behaviour';
+import {useQuestions} from './App.behaviour';
+import styles from './App.module.css';
 
 function App() {
 	const questions = useQuestions();
 
 	return (
-		<div>
-			<ul>
-				{questions.map(question => (
-					<li key={question.url}>{question.question}</li>
-				))}
-			</ul>
+		<div className={styles.container}>
+			<h1 className={styles.heading}>Questions</h1>
+			<div>
+				<ul className={styles.cardList}>
+					{questions.map(question => (
+						<li key={question.url} className={styles.card}>{question.question}</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 }
