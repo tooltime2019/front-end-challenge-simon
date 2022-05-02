dfd7425742ee187b0b4a1042fcdb56df38ece2ea
Simon Weller <s.meierbeck@gmail.com>
2022-05-02 10:01:45 +0200

    fetch and display questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+interface Choice {
+	choice: string,
+	votes: number,
+	url: string,
+}
+
+interface Question {
+	question: string,
+	choices: Choice[],
+	published_at: string,
+	url: string,
+}
 
 function App() {
-  return (
-    <div>
-      Hello world
-    </div>
-  );
+	const url = "https://polls.apiblueprint.org/questions"
+
+	const [questions, setQuestions] = useState<Question[]>([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			const data = await fetch(url);
+			const json = await data.json();
+			setQuestions(json)
+		}
+
+		void fetchData();
+	}, [])
+
+	return (
+		<div>
+			<ul>
+				{questions.map(question => (
+					<li>{question.question}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
